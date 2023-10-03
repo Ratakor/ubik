acdb17fb303205179472cac94f018752362e91b1
Ratakor <ratakor@disroot.org>
2023-10-03 15:58:33 +0200

    Move some x86_64 specific code to its own file

--- kernel/arch.zig
+++ kernel/arch.zig
@@ -1,62 +1,8 @@
-pub inline fn halt() noreturn {
-    while (true) asm volatile ("hlt");
-}
+pub const arch = @import("builtin").target.cpu.arch;
 
-pub inline fn disableInterrupts() void {
-    asm volatile ("cli");
-}
-
-pub inline fn enableInterrupts() void {
-    asm volatile ("sti");
-}
-
-pub inline fn out(comptime T: type, port: u16, value: T) void {
-    switch (T) {
-        u8 => asm volatile (
-            \\outb %[val], %[port]
-            :
-            : [val] "{al}" (value),
-              [port] "N{dx}" (port),
-            : "memory"
-        ),
-        u16 => asm volatile (
-            \\outw %[val], %[port]
-            :
-            : [val] "{ax}" (value),
-              [port] "N{dx}" (port),
-            : "memory"
-        ),
-        u32 => asm volatile (
-            \\outl %[val], %[port]
-            :
-            : [val] "{eax}" (value),
-              [port] "N{dx}" (port),
-            : "memory"
-        ),
-        else => @compileError("No port out instruction available for type " ++ @typeName(T)),
-    }
-}
-
-pub inline fn in(comptime T: type, port: u16) T {
-    return switch (T) {
-        u8 => asm volatile (
-            \\inb %[port], %[res]
-            : [res] "={al}" (-> T),
-            : [port] "N{dx}" (port),
-            : "memory"
-        ),
-        u16 => asm volatile (
-            \\inw %[port], %[res]
-            : [res] "={ax}" (-> T),
-            : [port] "N{dx}" (port),
-            : "memory"
-        ),
-        u32 => asm volatile (
-            \\inl %[port], %[res]
-            : [res] "={eax}" (-> T),
-            : [port] "N{dx}" (port),
-            : "memory"
-        ),
-        else => @compileError("No port in instruction available for type " ++ @typeName(T)),
-    };
-}
+pub usingnamespace switch (arch) {
+    .x86_64 => @import("arch/x86_64.zig"),
+    // .riscv64 => @import("arch/riscv64.zig"),
+    // .aarch64 => @import("arch/aarch64.zig"),
+    else => @compileError("Unsupported architecture: " ++ @tagName(arch)),
+};
---
+++ kernel/arch/x86_64.zig
@@ -0,0 +1,62 @@
+pub inline fn halt() noreturn {
+    while (true) asm volatile ("hlt");
+}
+
+pub inline fn disableInterrupts() void {
+    asm volatile ("cli");
+}
+
+pub inline fn enableInterrupts() void {
+    asm volatile ("sti");
+}
+
+pub inline fn out(comptime T: type, port: u16, value: T) void {
+    switch (T) {
+        u8 => asm volatile (
+            \\outb %[val], %[port]
+            :
+            : [val] "{al}" (value),
+              [port] "N{dx}" (port),
+            : "memory"
+        ),
+        u16 => asm volatile (
+            \\outw %[val], %[port]
+            :
+            : [val] "{ax}" (value),
+              [port] "N{dx}" (port),
+            : "memory"
+        ),
+        u32 => asm volatile (
+            \\outl %[val], %[port]
+            :
+            : [val] "{eax}" (value),
+              [port] "N{dx}" (port),
+            : "memory"
+        ),
+        else => @compileError("No port out instruction available for type " ++ @typeName(T)),
+    }
+}
+
+pub inline fn in(comptime T: type, port: u16) T {
+    return switch (T) {
+        u8 => asm volatile (
+            \\inb %[port], %[res]
+            : [res] "={al}" (-> T),
+            : [port] "N{dx}" (port),
+            : "memory"
+        ),
+        u16 => asm volatile (
+            \\inw %[port], %[res]
+            : [res] "={ax}" (-> T),
+            : [port] "N{dx}" (port),
+            : "memory"
+        ),
+        u32 => asm volatile (
+            \\inl %[port], %[res]
+            : [res] "={eax}" (-> T),
+            : [port] "N{dx}" (port),
+            : "memory"
+        ),
+        else => @compileError("No port in instruction available for type " ++ @typeName(T)),
+    };
+}
